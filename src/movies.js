0823498c2484e45ecd0afa3198bba8ca94a6ce30
Adrià Madueño <adrimadu19@gmail.com>
2022-07-27 17:35:13 +0200

    Finish Lab

--- src/movies.js
+++ src/movies.js
@@ -62,13 +62,24 @@ const orderByYear = (moviesArray) => {
     return orderYears;
 }
 
-console.log (orderByYear(movies));
+// console.log (orderByYear(movies));
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-function orderAlphabetically(moviesArray) {}
+const orderAlphabetically = (moviesArray) => {
+
+    const aplhabeticOrder = moviesArray.sort((movie, titles) => {
+        var textA = movie.title.toUpperCase();
+        var textB = titles.title.toUpperCase();
+        return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
+});
+    
+    return aplhabeticOrder.splice(0, 20);
+}
+
+console.log(orderAlphabetically(movies));
+
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
-function turnHoursToMinutes(moviesArray) {}
 
 // BONUS - Iteration 8: Best yearly score average - Best yearly score average
 function bestYearAvg(moviesArray) {}
